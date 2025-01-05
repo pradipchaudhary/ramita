043ecabcbb0c6309c79fb4fdcb7ecf1babe2bff3
Pradip Chaudhary <chaudharypradip678@gmail.com>
2025-01-05 22:26:28 +0545

    feat: change logo color

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,29 +15,42 @@ const Footer = () => {
                         <div className="mb-4">
                             <Logo />
                         </div>
-                        <h2 className="text-[34px] mb-[1rem]">
-                            Christina Koller
-                        </h2>
-                        <p className="mt-2 ">
+
+                        <p className="mt-2 pr-8 w-[86%]">
                             You can also list down your achievements or some
                             well-known clients you’ve worked for. Such details
                             often attract customers and encourage them to give
                             you a try.
                         </p>
+                        <div className="mt-10 ">
+                            <h2 className="text-[30px]">
+                                Subscribe For Offers
+                            </h2>
+                            <div className="flex w-full mt-10 pr-28">
+                                <input
+                                    type="email"
+                                    placeholder="Email Address"
+                                    className="flex-1 py-3 px-4 rounded-md"
+                                />
+                                <button className="bg-pink-500 hover:bg-pink-600 text-white text-lg font-semibold py-3 px-6 rounded-lg shadow-lg transition duration-300">
+                                    Subscribe
+                                </button>
+                            </div>
+                        </div>
                     </div>
 
                     <div className="flex-1 flex gap-8">
                         {/* Section 2: Quick Links */}
                         <div className="flex-1">
                             <div className=" mb-[28px]">
-                                <h2 className="text-[34px] mb-[28px]">
+                                <h2 className="text-[30px] mb-[28px]">
                                     Quick Links
                                 </h2>
-                                <ul className="space-y-2">
+                                <ul className="space-y-2 pl-2">
                                     <li>
                                         <a
                                             href="#services"
-                                            className="hover:text-blue-500"
+                                            className="hover:text-pink-500"
                                         >
                                             Services
                                         </a>
@@ -45,7 +58,7 @@ const Footer = () => {
                                     <li>
                                         <a
                                             href="#portfolio"
-                                            className="hover:text-blue-500"
+                                            className="hover:text-pink-500"
                                         >
                                             Portfolio
                                         </a>
@@ -53,7 +66,7 @@ const Footer = () => {
                                     <li>
                                         <a
                                             href="#about"
-                                            className="hover:text-blue-500"
+                                            className="hover:text-pink-500"
                                         >
                                             About Me
                                         </a>
@@ -61,7 +74,7 @@ const Footer = () => {
                                     <li>
                                         <a
                                             href="#contact"
-                                            className="hover:text-blue-500"
+                                            className="hover:text-pink-500"
                                         >
                                             Contact
                                         </a>
@@ -69,7 +82,7 @@ const Footer = () => {
                                 </ul>
                             </div>
                             <div>
-                                <h2 className="text-[34px] mb-[28px]">
+                                <h2 className="text-[30px] mb-[28px]">
                                     Follow me{" "}
                                 </h2>
                             </div>
@@ -78,15 +91,15 @@ const Footer = () => {
                         {/* Section 3: Contact Info */}
                         <div className="flex-1">
                             <div className="email mb-[47px]">
-                                <h2 className="text-[34px] mb-[28px]">
+                                <h2 className="text-[30px] mb-[28px]">
                                     Say Hi!
                                 </h2>
                                 <p className="">ramita@gmail.com</p>
                                 <p>info@ramitabeauty.com</p>
                             </div>
                             <div className="call mb-[47px]">
-                                <h2 className="text-[34px] mb-[28px]">
-                                    Say Hi!
+                                <h2 className="text-[30px] mb-[28px]">
+                                    Call Us
                                 </h2>
                                 <p className="">
                                     <span>Phone: </span>(+977) 9824 367 788
@@ -94,7 +107,7 @@ const Footer = () => {
                             </div>
 
                             <div className="call mb-[47px]">
-                                <h2 className="text-[34px] mb-[28px]">
+                                <h2 className="text-[30px] mb-[28px]">
                                     Find Us
                                 </h2>
                                 <p className="">
@@ -109,7 +122,7 @@ const Footer = () => {
             <div className="bg-[#1a1a1a] py-[95px] border border-t border-[#ffffff14]">
                 <div className="container mx-auto md:flex md:items-center md:justify-between">
                     <p className="">
-                        © 2025 Christina Koller. All rights reserved.
+                        © 2025 Ramita Beauty Parlor. All rights reserved.
                     </p>
                     <p className="">
                         Developed by{" "}
